cef9014ad942ee7ede4a3e54dc0014e21030101b
hemanthtavva <hemanthtavva1308@gmail.com>
2026-07-04 01:42:19 +0530

    append check

---
+++ random_1.py
@@ -0,0 +1,5 @@
+import random
+
+# print(random.random())  # Generates a random float between 0.0 and 1.0
+
+# print(random.randint(1, 10)) # Generates a random integer between 1 and 10 (inclusive)?
