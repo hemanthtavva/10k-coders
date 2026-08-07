65dbe054aa711ff4e48b89eb5a10e8e3ca7a6a36
hemanthtavva <hemanthtavva1308@gmail.com>
2026-08-07 22:39:11 +0530

    Remove pattern printing code and retain set operations example in patternn.py

--- patternn.py
+++ patternn.py
@@ -7,3 +7,10 @@ for i in range (1, rows+1):
         else:
             res += "  "
         print(res)
+
+# even if we want to uodate a dupilicate value it will not add same extra value
+nums = {1,2,3,4,5}
+nums.add(3)
+print(nums)
+nums.add(6)
+print(nums)
